13766bb3796771b0637b72321434daca7b10c03e
VinnyDSR <r.viniciusds@gmail.com>
2021-02-17 21:05:42 -0300

    upload1

---
+++ 1 Lógica de Programação/TesteLuby/1.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TesteLuby
+{
+    class Program
+    {
+        //  1.1 Implemente a função abaixo para calcular fatorial de um número.
+        static void Main(string[] args)
+        {
+            // 5! = 5 · 4 · 3 · 2 · 1 = 120
+            bool a = CalcularFatorial(5) == 120;//true
+            Console.WriteLine(a);
+        }
+
+        private static int CalcularFatorial(int v)
+        {
+            int aux = 1;
+           
+
+            for( ; v > 0; v--)
+            {
+                aux = v * aux;
+            }
+            
+            return aux;
+        }
+    }
+}
